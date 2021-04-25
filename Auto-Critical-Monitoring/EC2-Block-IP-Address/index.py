6ac1cba8611e8f5c27c57e526c74ca67b7c5e5ee
Sophia Vu <41712745+hsvu@users.noreply.github.com>
2021-04-25 22:48:20 +1000

    addeding comments

--- Auto-Critical-Monitoring/EC2-Block-IP-Address/index.py
+++ Auto-Critical-Monitoring/EC2-Block-IP-Address/index.py
@@ -1,3 +1,14 @@
+# How this lambda function works
+# Lambda function takes two variables as the payload - the EC2 instance id and the IP address to block.
+# Using the instance id, we need to find the network ACL which this instance uses.
+# this is because the network ACL defines inbound and outbound rules aka.
+# what is allowed to use this instance and what is allowed to come out of this instance
+# We do this by using the instance Id to get the Vpc id, and then using the Vpc id to get the network ACL id.
+# From here, we create a new inbound rule. 
+# Most of these function calls are made from boto3. 
+# See https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html for more information
+# on how each function works.
+
 import boto3
 import hashlib
 import json
@@ -5,20 +16,12 @@ from botocore.exceptions import ClientError
 import urllib3
 http = urllib3.PoolManager()
 
-# ID of the security group we want to update
-# SECURITY_GROUP_ID = "sg-XXXX"
-
-# Description of the security rule we want to replace
-# SECURITY_RULE_DESCR = "My Home IP"
-    
+# function looks to see if IP subnet is given 
+# i.e. checks to see if there is a "/" in the given IP address
+# if no "/" is given, then we assume that only a single IP address is to be blocked 
+# i.e. adding a /32 to the end of the IP address
 def gen_iprange(ip_addr, i = 32):
     if(ip_addr.find("/") == -1): # hash not found
-        # i - Cisco style mark (or whatever it's called)
-        #  * 32 means to specify 1 IP address only
-        #  * 24 means to specify the entire subnet (e.g. 192.168.10.* )
-        #  * 0 means to block the entire IPv4 range ie the internet (lol)
-        #  Too sleepy to remember the name for the thing, but it's calculated as,
-        #  `2^(32-i)` many IPv4 addresses (to specify) 
         buf = "{:s}/{:d}".format(ip_addr, i)
     else:
         buf = ip_addr
@@ -27,6 +30,8 @@ def gen_iprange(ip_addr, i = 32):
     
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
+    
+    # using the given instance id, find the associated VPC id.
     vpc = ec2.describe_instances(
         InstanceIds=[
             event['id']
@@ -34,6 +39,7 @@ def lambda_handler(event, context):
     )['Reservations'][0]['Instances'][0]['VpcId']
     print(vpc)
     
+    # using the given VPC id, find the associated network ACL id.
     networkACL = ec2.describe_network_acls(
         Filters=[
             {
@@ -46,6 +52,8 @@ def lambda_handler(event, context):
     )['NetworkAcls'][0]['Associations'][0]['NetworkAclId']
     print(networkACL)
     
+    # finding out how many inbound and outbound rules are already
+    # made in the given network ACL
     entryCount = len(ec2.describe_network_acls(
         Filters=[
             {
@@ -57,6 +65,10 @@ def lambda_handler(event, context):
         ],
     )['NetworkAcls'][0]['Entries'])
     
+    # randomising the rule number, making sure that theres enough space
+    # between rule numbers to add more rules later on if needed (hence multiplying it by 5)
+    # we do this because the rule number needs to be unique.
+    # by default, the only rule number used in a network ACL is 100.
     ruleNum = entryCount*5
     
     ingress = ec2.create_network_acl_entry(
